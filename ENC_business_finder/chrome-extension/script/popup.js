de04ed2f560721bf39def1b5330e073b1228f3e2
bluefishgeneral <garadadil@hotmail.com>
2021-03-09 13:57:26 -0500

    commented popup.js

--- ENC_business_finder/chrome-extension/script/popup.js
+++ ENC_business_finder/chrome-extension/script/popup.js
@@ -2,6 +2,10 @@ let websites = [];
 let bnames = [];
 let MAX_PAGES, email, email_app_pass, db_url, start_b;
 
+
+/*
+ In the current tab, inject the find.js script. The find.js script is responsible for finding businesses and their websites in a given area and exporting them to a JSON file. 
+*/
 function injectTheScript() {
     // Gets all tabs that have the specified properties, or all tabs if no properties are specified (in our case we choose current active tab)
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -11,14 +15,12 @@ function injectTheScript() {
 }
 
 
-
-
-
-//start_b = document.getElementById('start_b').value;
-//console.log(start_b);
-//start_b.onclick = function(){start();};
 chrome.browserAction.onClicked.addListener(function(tab) { start();});
 
+
+/*
+ Start the extension's main function.
+*/
 function start(){
 injectTheScript();
 
